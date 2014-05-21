df54124fb375f62b28a772e9bbbea3e984a66698
Mauro Bianchi <bianchimro@gmail.com>
2014-05-21 14:07:22 +0200

    removed platforms from git

--- platforms/ios/www/lib/angular-ol3-inmagik/src/ng-ol3-inmagik-base.js
+++
@@ -1,5 +0,0 @@
-(function () {
-
-  angular.module ('ngOL3Inmagik', []);
-
-}) ();
--- platforms/ios/www/lib/pouchdb/bin/get-version.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-console.log(JSON.stringify(require('../package.json').version));
